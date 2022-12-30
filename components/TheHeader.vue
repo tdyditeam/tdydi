0fdbf2ed4a223b5158da2ae1608068874f2c6c6e
Oraz Durdyyev <o.durdyyev@salam.tm>
2022-12-30 21:19:21 +0500

    header update

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -11,16 +11,175 @@
         <img src="@/assets/icons/logo.png" alt="" />
       </div>
       <div class="header__top-content">
-        <div class="languages">TM</div>
+        <div class="languages">
+          <span class="_active">TM</span>
+          <span>RU</span>
+          <span>EN</span>
+        </div>
         <div class="contact">Contact</div>
       </div>
     </div>
-    <div class="header__bottom"></div>
+    <div class="header__bottom">
+      <ul class="header__bottom-menu">
+        <li v-for="item in menu" :key="item.id" class="header__bottom-items">
+          <span>{{ item.name }}</span>
+          <span></span>
+          <ul class="header__bottom-submenu">
+            <li
+              v-for="elem in item.subMenus"
+              :key="elem.id"
+              class="header__bottom-subitems"
+            >
+              <span>{{ elem.name }}</span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      menu: [
+        {
+          id: 1,
+          name: 'Baş sahypa',
+          subMenus: [
+            {
+              id: 1,
+              name: 'Rektorat',
+            },
+            {
+              id: 2,
+              name: 'Fakultetler',
+            },
+            {
+              id: 3,
+              name: 'Kafedralar',
+            },
+            {
+              id: 4,
+              name: 'Bölümler',
+            },
+            {
+              id: 5,
+              name: 'Institutyň taryhy',
+            },
+            {
+              id: 6,
+              name: 'Institutyň TDP',
+            },
+            {
+              id: 7,
+              name: 'Kärdeşler arkalaşygy',
+            },
+            {
+              id: 8,
+              name: 'Talyp ýaşlar guramasy',
+            },
+            {
+              id: 9,
+              name: 'Zenanlar guramasy',
+            },
+            {
+              id: 10,
+              name: 'Kitaphana',
+            },
+            {
+              id: 11,
+              name: 'Ykdysatçy okuw merkezi',
+            },
+            {
+              id: 11,
+              name: 'Ykdysatçy okuw merkezi',
+            },
+            {
+              id: 11,
+              name: 'Ykdysatçy okuw merkezi',
+            },
+            {
+              id: 11,
+              name: 'Ykdysatçy okuw merkezi',
+            },
+            {
+              id: 11,
+              name: 'Ykdysatçy okuw merkezi',
+            },
+            {
+              id: 11,
+              name: 'Ykdysatçy okuw merkezi',
+            },
+            {
+              id: 11,
+              name: 'Ykdysatçy okuw merkezi',
+            },
+          ],
+        },
+        {
+          id: 2,
+          name: 'Biz barada',
+          subMenus: [
+            {
+              id: 1,
+              name: 'Rektorat',
+            },
+            {
+              id: 2,
+              name: 'Fakultetler',
+            },
+            {
+              id: 3,
+              name: 'Kafedralar',
+            },
+            {
+              id: 4,
+              name: 'Bölümler',
+            },
+            {
+              id: 5,
+              name: 'Institutyň taryhy',
+            },
+            {
+              id: 6,
+              name: 'Institutyň TDP',
+            },
+            {
+              id: 7,
+              name: 'Kärdeşler arkalaşygy',
+            },
+            {
+              id: 8,
+              name: 'Talyp ýaşlar guramasy',
+            },
+          ],
+        },
+        {
+          id: 3,
+          name: 'Bilim',
+        },
+        {
+          id: 4,
+          name: 'Ylym ',
+        },
+        {
+          id: 5,
+          name: 'Halkara hyzmatdaşlygy',
+        },
+        {
+          id: 6,
+          name: 'Dalaşgär-2022',
+        },
+        {
+          id: 7,
+          name: 'Bäsleşikler-2022',
+        },
+      ],
+    }
+  },
+}
 </script>
 
 <style lang="scss">
@@ -31,7 +190,7 @@ export default {}
     &-title {
       h2 {
         padding: 29px 0;
-        font-style: normal;
+        font-family: 'Gilroy';
         font-weight: 700;
         font-size: 20px;
         line-height: 23px;
@@ -52,14 +211,141 @@ export default {}
     &-content {
       display: flex;
       align-items: center;
+      font-family: 'Gilroy';
+      font-style: normal;
+      font-weight: 600;
+      font-size: 18px;
+      line-height: 21px;
+      text-align: center;
+      text-transform: capitalize;
+
+      color: #333333;
       .languages {
-      }
-      .contact {
-        margin-left: 15px;
+        display: flex;
+        cursor: pointer;
         position: relative;
+        width: 150px;
+        transition: 2s ease;
+        height: 25px;
+        padding: 3px 0;
+        &:hover span {
+          &:nth-child(1) {
+            transform: translateX(-75px);
+          }
+          &:nth-child(2) {
+            transform: translateX(-37px);
+          }
+          opacity: 1;
+          visibility: visible;
+        }
+        span {
+          position: absolute;
+          right: 0;
+          //   top: 50%;
+          //   transform: translateY(-50%);
+          opacity: 0;
+          visibility: hidden;
+          transition: 0.3s ease;
+          &:hover {
+            color: var(--primary);
+          }
+          &._active {
+            opacity: 1;
+            visibility: visible;
+            color: var(--primary);
+          }
+        }
+      }
+
+      .contact {
+        margin-left: 35px;
+        position: relative;
+        cursor: pointer;
         &::after {
           content: '';
           position: absolute;
+          left: -15px;
+          width: 1px;
+          height: 100%;
+          background: #333333;
+        }
+      }
+    }
+  }
+  &__bottom {
+    width: 100%;
+    &-menu {
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      width: 100%;
+      padding: 10px 0 14px 0;
+      font-family: 'Gilroy';
+      font-weight: 600;
+      font-size: 18px;
+      line-height: 21px;
+      position: relative;
+    }
+    &-items {
+      cursor: pointer;
+      transition: 0.3s ease;
+      display: flex;
+      flex-direction: column;
+      span:nth-child(2) {
+        margin-top: 14px;
+        height: 2px;
+        width: 0%;
+        transition: 0.3s ease;
+        background: var(--primary);
+        border-radius: 30px;
+      }
+      &:hover {
+        color: var(--primary);
+        & span:nth-child(2) {
+          width: 100%;
+        }
+      }
+      &:hover .header__bottom-submenu {
+        opacity: 1;
+        visibility: visible;
+      }
+    }
+    &-submenu {
+      position: absolute;
+      left: 0;
+      display: flex;
+      width: 100%;
+      top: 48px;
+      color: var(--text);
+      border-top: 2px solid var(--border);
+      opacity: 0;
+      visibility: hidden;
+      transition: 0.3s ease;
+      overflow: auto;
+    }
+    &-subitems {
+      padding: 10px 0;
+      margin-left: 15px;
+      display: flex;
+      align-items: center;
+      position: relative;
+      transition: 0.3s ease;
+      margin-left: 25px;
+      &:not(:last-child) {
+        &::after {
+          position: absolute;
+          content: '';
+          top: 50%;
+          transform: translateY(-50%);
+          right: -15px;
+          background: var(--primary);
+          height: 25%;
+          width: 1px;
+        }
+      }
+      span {
+        &:hover {
+          color: var(--primary);
         }
       }
     }
