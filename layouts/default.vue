d7699cbaa977babb05e65116208676887cf20fa1
Muhammetmyrat <bayramovmuhammetmyrat97@gmail.com>
2023-01-10 14:58:10 +0500

    last

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,4 +10,12 @@
 export default {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.wrapper {
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+</style>
