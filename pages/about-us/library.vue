88094548089955f818706a75f156795d77b2ead4
Muhammetmyrat <bayramovmuhammetmyrat97@gmail.com>
2023-02-23 14:19:58 +0500

    last

--- pages/about-us/library.vue
+++ pages/about-us/library.vue
@@ -13,85 +13,113 @@ export default {
         {
           id: 1,
           name: this.$t('departments.accounting.title'),
-          path: '/about-us/library/accounting',
+          path: `/about-us/library/accounting?q=${this.$t(
+            'departments.accounting.title'
+          )}`,
           exact: false,
         },
         {
           id: 2,
           name: this.$t('departments.statistics.title'),
-          path: '/about-us/library/statistic',
+          path: `/about-us/library/statistic?q=${this.$t(
+            'departments.statistics.title'
+          )}`,
           exact: false,
         },
         {
           id: 3,
           name: this.$t('departments.economicsandlaw.title'),
-          path: '/about-us/library/economical-and-law',
+          path: `/about-us/library/economical-and-law?q=${this.$t(
+            'departments.economicsandlaw.title'
+          )}`,
           exact: false,
         },
         {
           id: 4,
           name: this.$t('departments.economictheory.title'),
-          path: '/about-us/library/economic',
+          path: `/about-us/library/economic?q=${this.$t(
+            'departments.economictheory.title'
+          )}`,
           exact: false,
         },
         {
           id: 5,
           name: this.$t('departments.banking.title'),
-          path: '/about-us/library/banking',
+          path: `/about-us/library/banking?q=${this.$t(
+            'departments.banking.title'
+          )}`,
           exact: false,
         },
         {
           id: 6,
           name: this.$t('departments.finance.title'),
-          path: '/about-us/library/finance',
+          path: `/about-us/library/finance?q=${this.$t(
+            'departments.finance.title'
+          )}`,
           exact: false,
         },
         {
           id: 7,
           name: this.$t('departments.digitalEconomy.title'),
-          path: '/about-us/library/digital-ecocnomy',
+          path: `/about-us/library/digital-ecocnomy?q=${this.$t(
+            'departments.digitalEconomy.title'
+          )}`,
           exact: false,
         },
         {
           id: 9,
           name: this.$t('departments.globalEconomy.title'),
-          path: '/about-us/library/world-economy',
+          path: `/about-us/library/world-economy?q=${this.$t(
+            'departments.globalEconomy.title'
+          )}`,
           exact: false,
         },
         {
           id: 10,
           name: this.$t('departments.management.title'),
-          path: '/about-us/library/menejment',
+          path: `/about-us/library/menejment?q=${this.$t(
+            'departments.management.title'
+          )}`,
           exact: false,
         },
         {
           id: 11,
           name: this.$t('departments.informationsystems.title'),
-          path: '/about-us/library/information-systems',
+          path: `/about-us/library/information-systems?q=${this.$t(
+            'departments.informationsystems.title'
+          )}`,
           exact: false,
         },
         {
           id: 12,
           name: this.$t('departments.foreignLanguages.title'),
-          path: '/about-us/library/foreign-languages',
+          path: `/about-us/library/foreign-languages?q=${this.$t(
+            'departments.foreignLanguages.title'
+          )}`,
           exact: false,
         },
         {
           id: 13,
           name: this.$t('departments.marketing.title'),
-          path: '/about-us/library/marketing',
+          path: `/about-us/library/marketing?q=${this.$t(
+            'departments.marketing.title'
+          )}`,
           exact: false,
         },
         {
           id: 14,
           name: this.$t('departments.economicsLabor.title'),
-          path: '/about-us/library/economics-of-labor',
+          path: `/about-us/library/economics-of-labor?q=${this.$t(
+            'departments.economicsLabor.title'
+          )}`,
           exact: false,
         },
         {
           id: 15,
           name: this.$t('departments.mathematics.title'),
-          path: '/about-us/library/advanced-mathematics-and-computer-science',
+          path: `/about-us/library/advanced-mathematics-and-computer-science?q=${this.$t(
+            'departments.mathematics.title'
+          )}`,
           exact: false,
         },
       ],
