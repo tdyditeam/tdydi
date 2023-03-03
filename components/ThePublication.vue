4e68795d435c7954904ad1c3ca12d2523b102ad6
Muhammetmyrat <bayramovmuhammetmyrat97@gmail.com>
2023-03-03 14:23:09 +0500

    publications get

--- components/ThePublication.vue
+++ components/ThePublication.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="publication">
     <the-title-block :title="$t('button.publications')"></the-title-block>
-    <the-publication-swiper></the-publication-swiper>
+    <the-publication-swiper :images="publications"></the-publication-swiper>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    publications: {
+      type: Array,
+      default: () => [],
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped>
