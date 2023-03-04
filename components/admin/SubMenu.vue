e00722b167bace08c76447171e39b2efd54a497d
Oraz Durdyyev <o.durdyyev@salam.tm>
2023-03-04 15:33:22 +0500

    update admin about page

--- components/admin/SubMenu.vue
+++ components/admin/SubMenu.vue
@@ -2,16 +2,10 @@
   <div v-if="items.length" class="admin-submenu-wrapper">
     <ul class="admin-submenu-list">
       <client-only v-for="item in items" :key="item.id">
-        <nuxt-link :to="item.path" class="admin-submenu-item">{{
+        <nuxt-link exact :to="item.path" class="admin-submenu-item">{{
           item.name
         }}</nuxt-link>
       </client-only>
-
-      <!-- <nuxt-link to="article" class="admin-submenu-item">Makalalar</nuxt-link>
-      <li class="admin-submenu-item">Täzelikler</li>
-      <li class="admin-submenu-item">Neşirler</li>
-      <li class="admin-submenu-item">Gallery</li>
-      <li class="admin-submenu-item">Hyzmatdaşlar</li> -->
     </ul>
   </div>
 </template>
---
+++ pages/admin/about-us/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div style="height: calc(100% - 80px)">
+    <sub-menu :items="subMenu"></sub-menu>
+    <nuxt-child></nuxt-child>
+  </div>
+</template>
+
+<script>
+const SubMenu = () => import('~/components/admin/SubMenu.vue')
+
+export default {
+  layout: 'admin',
+  middleware(app) {
+    // return app.redirect(app.app.localePath('/admin/main/slider'))
+  },
+  components: {
+    SubMenu,
+  },
+  data() {
+    return {
+      subMenu: [
+        {
+          id: 1,
+          name: 'Biz barada',
+          path: '/admin/about-us/',
+        },
+        {
+          id: 2,
+          name: 'Rektorat',
+          path: '/admin/about-us/rektorat',
+        },
+        {
+          id: 3,
+          name: 'Fakulteltar',
+          path: '/admin/about-us/fakulties',
+        },
+        {
+          id: 4,
+          name: 'Kafedralar',
+          path: 'publications',
+        },
+        {
+          id: 5,
+          name: 'Jemgyýetçilik guramalary',
+          path: 'gallery',
+        },
+        {
+          id: 6,
+          name: 'Okuw merkezleri',
+          path: 'partners',
+        },
+        {
+          id: 7,
+          name: 'Kitaphana',
+          path: 'indicators',
+        },
+        {
+          id: 8,
+          name: 'Hazyna çeper höwesjeňler merkezi',
+          path: 'indicators',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
