f6cf5202551a6458f45468ac7ec2a0099aac8768
Muhammetmyrat <bayramovmuhammetmyrat97@gmail.com>
2023-04-25 09:30:15 +0500

    last

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="wrapper">
-    <div class="loader" v-if="isLoaded">
+    <div class="loader" v-show="isLoaded">
       <Loader />
     </div>
     <the-header-second></the-header-second>
--- store/index.js
+++ store/index.js
@@ -14,6 +14,7 @@ export const getters = {
 
 export const mutations = {
   SET_LOADER(state, val) {
+    console.log('SET_LOADER')
     state.isLoaded = val
   },
 }
